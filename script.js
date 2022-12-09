f493ab6c9356fcdf4ac0ecd7bd34621137128b05
Adrian Olariu <adrian.olariu93@gmail.com>
2022-12-09 21:10:40 +0200

    navbar edit + front girl edit

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ var root = getComputedStyle(rootCss);//process those variables
 lightbulb.addEventListener('click', (e) =>{
 
     if(lightbulb.src.includes('off')){
-        girlPic.style.content='url(./src/Girl_on.jpg)';
+        girlPic.style.content='url(./src/lithophane_girl_front_on4.png)';
         
         lightbulb.src='./src/Light_on_768x768.png';
         document.body.style.backgroundColor = root.getPropertyValue('--bgLight');
     }else{
-        girlPic.style.content='url(./src/Girl_off.jpg)';
+        girlPic.style.content='url(./src/lithophane_girl_front_off.png)';
 
         lightbulb.src='./src/Light_off_768x768.png';
         document.body.style.backgroundColor = root.getPropertyValue('--bgDark');
