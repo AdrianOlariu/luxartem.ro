539e6ab0d2c109ddd44c8fcf029eb789b4da04d7
Adrian Olariu <adrian.olariu93@gmail.com>
2022-12-11 19:57:04 +0200

    Router implementation Overall style defined

---
+++ router.js
@@ -0,0 +1,62 @@
+//ROUTER FUNCTIONALITY:
+////INFO: For the router to work properly, a div with the id corresponding to the nav link has to be
+//added in the index page. ex. <div id="shop"> </div> as well as its selector from the ul navigation
+// header: <li><a href="#">shop</a></li>.
+//This router is dinamically generated based on the -links- from the -navigation header-.
+//The -value- of the -links- is being used to generate -query selector- and to -add event listener-
+//for each link.
+//The -event listener- calls the -show page function- that will show the section with its name from
+//which was clicked.
+//The -show page function - goes through all the -sections- and -adds the hide class- to every
+//page and -removes the hide class- from the page that matches the name from which the function
+//was clicked.
+
+
+let pages = [];
+const navLinks = document.querySelector('.nav');
+
+generateNavLinksSectionIDs(
+    () => showPage('home')
+)();
+
+function generateNavLinksSectionIDs(callback){
+
+    // const home = document.querySelector('#home');
+    // const collaboration = document.querySelector('#collaboration');
+    // const shop = document.querySelector('#shop');
+    // const contact = document.querySelector('#contact');
+    // pages.push(home,shop,contact);
+
+    let pagesName = ['home'];//because this link does not exists in navlinks
+    navLinks.childNodes.forEach(val =>{
+        if(val.lastChild != null){
+            console.log(val.lastChild);
+            val.lastChild.classList.add('section')
+            val.lastChild.addEventListener('click', (e)=>{
+                e.preventDefault();
+                console.log('clicked',val.lastChild.textContent);
+                showPage(val.lastChild.textContent);
+            })
+            pagesName.push(val.lastChild.textContent);
+        }
+    });
+
+    for(id in pagesName){
+        pages.push(document.querySelector(`#${pagesName[id]}`))
+
+        //add general style for each section.
+        pages[id].classList.add('section');
+    }
+    return callback;
+    
+}
+
+function showPage(pageName){
+    pages.forEach((pg) => {
+        pg.classList.add('hide');
+        if(pg.id === pageName){
+            pg.classList.remove('hide');
+            console.log(pg);
+        }
+    });
+}
--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const girlPic = document.querySelector('.girl');
 const lightbulb = document.querySelector('.lightbulb');
-
+const luxartem = document.querySelector('.luxartem');
 
 var rootCss = document.querySelector(':root');//get the variables created in the css file
 var root = getComputedStyle(rootCss);//process those variables
 
+luxartem.addEventListener('click', (e)=>{
+    lights();
+})
+
 lightbulb.addEventListener('click', (e) =>{
 
+    lights();
+})
+
+function lights(){
     if(lightbulb.src.includes('off')){
         girlPic.style.content='url(./src/lithophane_girl_front_on4.png)';
-        
+
         lightbulb.src='./src/Light_on_768x768.png';
         document.body.style.backgroundColor = root.getPropertyValue('--bgLight');
+        document.documentElement.style.setProperty('--textDark', 'rgb(255, 255, 255)');
+        document.documentElement.style.setProperty('--textLight', 'rgb(0,0,0)');
     }else{
         girlPic.style.content='url(./src/lithophane_girl_front_off.png)';
 
         lightbulb.src='./src/Light_off_768x768.png';
         document.body.style.backgroundColor = root.getPropertyValue('--bgDark');
+        document.documentElement.style.setProperty('--textDark', 'rgb(186,187,189)');
+        document.documentElement.style.setProperty('--textLight', 'rgb(186,187,189)');
     }
-})
+}
 
-console.log('ceva');
+// console.log(pages[1]);
+
+// pages[1].addEventListener('click', (e)=>{
+//     e.preventDefault();
+//     console.log('home clicked');
+// })
