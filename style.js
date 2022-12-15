4521a6299971e7f650a7e36e0e9ef408b00e8507
Adrian Olariu <adrian.olariu93@gmail.com>
2022-12-15 03:39:26 +0200

    logo luxartem small visual fix

--- style.js
+++ style.js
@@ -3,9 +3,11 @@ const lightbulb = document.querySelector('.lightbulb');
 const luxartem = document.querySelector('.luxartem');
 const video = document.querySelector('.video-container');
 const divArrowDown = document.querySelector('.div-arrow-down');
+const divArrowUp = document.querySelector('.div-arrow-up');
 const rootCss = document.querySelector(':root');//get the variables created in the css file
 const root = getComputedStyle(rootCss);//process those variables
-
+const lightOrNight = document.querySelector('.light-or-night');
+let actions = 0;
 initWebsite();
 
 // luxartem.addEventListener('click', (e)=>{
@@ -14,7 +16,36 @@ initWebsite();
 
 lightbulb.parentElement.addEventListener('click', (e) =>{
     lights();
+    actions += 1;
+    if(actions > 2 ){
+        cookieSet('LightNight',true,60*24*7);
+        divArrowUp.classList.add('hide');
+        if(parseInt(cookieGetValue('ScrolledDown')) < 2 || cookieGetValue('ScrolledDown') === null ){
+            divArrowDown.classList.remove('hide');
+        }
+    }
 });
+
+function hideGuideMessages(){
+    //we show the user the arrow up message to make him/her understand that the website can be lit up
+    //we remove the message after he/she switches 2 times
+    //the value is saved in a cookie, next time, the message won't appear anymore
+
+    //after the discovery of the switch system, the message scroll down appears.
+    //after 3 scrolls, this message dissapears as well.
+    //the value is saved in a cookie
+    if(cookieGetValue('LightNight') === 'true'){
+        divArrowUp.classList.add('hide');
+        divArrowDown.classList.remove('hide');
+    }else{
+        divArrowDown.classList.add('hide');
+    }
+
+    if(parseInt(cookieGetValue('ScrolledDown')) > 2){
+        divArrowDown.classList.add('hide');
+        console.log(cookieGetValue('ScrolledDown'));
+    }
+}
 
 function lights(){
     if(lightbulb.src.includes('off')){
@@ -41,6 +72,8 @@ function darkTheme(){
     video.style.animation = 'to-gray 2s';
     video.style.filter = 'grayscale(100%)';
     highlightUnClicked(luxartem);
+    lightOrNight.innerHTML = " light";
+
 }
 
 function lightTheme(){
@@ -54,6 +87,8 @@ function lightTheme(){
     video.style.animation = 'to-color 2s';
     video.style.filter = 'grayscale(0%)';
     highlightClicked(luxartem);
+    lightOrNight.innerHTML = " night";
+
 
 }
 //Setam un cookie in care afisam de cate ori a rulat animatia cu breathing text logo.
@@ -80,6 +115,7 @@ function initWebsite(){
     luxartemTheme();
     breathingLUXARTEM(3,30);
     onScroll(video.parentElement);
+    hideGuideMessages();
     console.log(video.parentElement);
 }
 
@@ -103,14 +139,17 @@ function onScroll(element){
         console.log('scroll');
         onwheel = (event) => {
             if(!element.classList.contains('hide')){
+                
                 if(scrolled){
                     console.log('sageata ascunsa');
                     divArrowDown.classList.add('hide');
+                    if(parseInt(cookieGetValue('ScrolledDown')) < 3 || !cookieGetValue('ScrolledDown')){
+                        cookieSet('ScrolledDown',cookieGetValue('ScrolledDown') ? (parseInt(cookieGetValue('ScrolledDown')) + 1) : 1 , 60*24);
+                    }
                 }
                 if(event.deltaY < 0){
                     for(let i = scroolValue; i < 51; i+=0.5){
                         document.documentElement.style.setProperty('--scrollVideo', `${i}%`);
-                        
                         
                     }
                     document.documentElement.style.setProperty('--scrollImage', `${0}%`);
@@ -121,7 +160,7 @@ function onScroll(element){
                     
                 }else{
 
-                    for(let i = scroolValue; i > -85; i-=1){
+                    for(let i = scroolValue; i > -95; i-=1){
                         scrolled = true;
                         document.documentElement.style.setProperty('--scrollVideo', `${i}%`);
                     }
